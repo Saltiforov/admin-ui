5293b9e3d66b9078d34f758f3bae571f2493ed00
m.vynohradchii <m.vynohradchii@viseven.com>
2025-01-18 19:51:12 +0200

    add basic case

--- src/App.vue
+++ src/App.vue
@@ -3,13 +3,67 @@
     <Accordion :value="['0']" multiple>
       <AccordionPanel value="0">
         <AccordionHeader>Header I</AccordionHeader>
+
+
+
         <AccordionContent>
-          <p class="m-0">
-            Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo
-            consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.
-          </p>
+            <p>1</p>
+          <AccordionPanel value="3">
+            <AccordionHeader>CHILD 1</AccordionHeader>
+            <AccordionContent>
+              <p>SUB SUB CHILD</p>
+              <AccordionPanel value="13">
+                <AccordionHeader>CHILD 13</AccordionHeader>
+                <AccordionContent>
+                  <p class="m-0">
+                    Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo
+                    consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.
+                  </p>
+                </AccordionContent>
+              </AccordionPanel>
+              <AccordionPanel value="14">
+                <AccordionHeader>CHILD 14</AccordionHeader>
+                <AccordionContent>
+                  <p class="m-0">
+                    Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo
+                    consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.
+                  </p>
+                </AccordionContent>
+              </AccordionPanel>
+              <AccordionPanel value="15">
+                <AccordionHeader>CHILD 15</AccordionHeader>
+                <AccordionContent>
+                  <p class="m-0">
+                    Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo
+                    consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.
+                  </p>
+                </AccordionContent>
+              </AccordionPanel>
+            </AccordionContent>
+          </AccordionPanel>
+          <AccordionPanel value="6">
+            <AccordionHeader>CHILD 2</AccordionHeader>
+            <AccordionContent>
+              <p class="m-0">
+                Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo
+                consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.
+              </p>
+            </AccordionContent>
+          </AccordionPanel>
+          <AccordionPanel value="7">
+            <AccordionHeader>CHILD 3</AccordionHeader>
+            <AccordionContent>
+              <p class="m-0">
+                Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo
+                consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.
+              </p>
+            </AccordionContent>
+          </AccordionPanel>
         </AccordionContent>
       </AccordionPanel>
+
+
+
       <AccordionPanel value="1">
         <AccordionHeader>Header II</AccordionHeader>
         <AccordionContent>
