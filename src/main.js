f9a51eebad7294962212f254e4617d74fe4d9283
m.vynohradchii <m.vynohradchii@viseven.com>
2025-01-18 19:35:49 +0200

    add primevue to the project

--- src/main.js
+++ src/main.js
@@ -2,9 +2,22 @@ import { createApp } from 'vue'
 import './style.css'
 import App from './App.vue'
 import fetchPlugin from './plugins/fetch';
+import PrimeVue from "primevue/config";
+import Noir from "@/presets/Noir.js";
 
 const app = createApp(App);
+
 app.use(fetchPlugin);
+app.use(PrimeVue, {
+    theme: {
+        preset: Noir,
+        options: {
+            prefix: 'p',
+            darkModeSelector: '.p-dark',
+            cssLayer: false,
+        }
+    }
+});
 export default app;
 
 app.mount('#app');
--- vite.config.js
+++ vite.config.js
@@ -1,10 +1,19 @@
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
 import path from 'path';
+import Components from 'unplugin-vue-components/vite';
+import { PrimeVueResolver } from '@primevue/auto-import-resolver';
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [vue()],
+  plugins: [
+      vue(),
+    Components({
+      resolvers: [
+        PrimeVueResolver()
+      ]
+    })
+  ],
   resolve: {
     alias: {
       '@': path.resolve(__dirname, './src'), // Указываем путь к папке src
